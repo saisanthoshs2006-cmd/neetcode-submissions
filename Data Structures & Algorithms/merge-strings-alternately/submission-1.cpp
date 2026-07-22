0435f6e5b96230a757f06befd4d99af59b166251
SAI SANTHOSH S <saisanthoshs2006@gmail.com>
2026-07-22 18:19:40 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0;
+        int j=0;
+        int n=word1.size();
+        int m=word2.size();
+
+        string ans = "";
+
+        while(i<n && j<m){
+            ans.push_back(word1[i]);
+            ans.push_back(word2[j]);
+            i++;
+            j++;
+        }
+        while(i<n){
+            ans.push_back(word1[i]);
+            i++;
+        }
+        while(j<m){
+            ans.push_back(word2[j]);
+            j++;
+        }
+
+        return ans;
+    }
+};
