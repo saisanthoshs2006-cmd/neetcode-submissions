176eb8bfe4a968e0217f79be2d877257f04ca41c
SAI SANTHOSH S <saisanthoshs2006@gmail.com>
2026-07-25 11:25:30 +0530

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        if(n==m){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<i;j++){
+                    swap(mat[i][j],mat[j][i]);
+                }
+            }
+
+            return mat;
+        }
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[j][i] = mat[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
