d62314a4a10cb6a12f83ed708398c9d0238b331b
SAI SANTHOSH S <saisanthoshs2006@gmail.com>
2026-09-06 07:46:01 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set<int> st;
+        for(auto i:nums){
+            if(st.count(i)) return true;
+            st.insert(i);
+        }
+        return false;
+    }
+};
